e880babd5408dbb92070d05dda24e997fe7a203e
SoulSeekkor <soulseekkor@live.com>
2024-06-09 12:05:03 -0500

    Updated to listen on any IP by default.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 using NginxPanel.Services;
 using Radzen;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+	serverOptions.Listen(IPAddress.Any, 5000);
+	//serverOptions.Listen(IPAddress.Loopback, 5001, listenOptions =>
+	//{
+	//	listenOptions.UseHttps("testCert.pfx", "testPassword");
+	//});
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -18,9 +28,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
