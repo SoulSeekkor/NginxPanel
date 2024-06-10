7ec173486d745243f696ed3c117ac63e6f4d6eab
SoulSeekkor <soulseekkor@live.com>
2024-06-10 12:59:22 -0500

    Work on making the install Nginx functional.

--- Services/Nginx.cs
+++ Services/Nginx.cs
@@ -2,256 +2,261 @@
 
 namespace NginxPanel.Services
 {
-    public class Nginx
-    {
-        #region Classes
+	public class Nginx
+	{
+		#region Classes
 
-        public class ConfigFile
-        {
-            private string _fileContents = string.Empty;
+		public class ConfigFile
+		{
+			private string _fileContents = string.Empty;
 
-            public string Name = string.Empty;
-            public string ConfigPath = string.Empty;
+			public string Name = string.Empty;
+			public string ConfigPath = string.Empty;
 
-            public bool Enabled = false;
-            public string FileContents
-            {
-                get { return _fileContents; }
-                set {
-                    _fileContents = value;
-                    ContentsDirty = true;
-                }
-            }
-            public bool ContentsDirty = false;
+			public bool Enabled = false;
+			public string FileContents
+			{
+				get { return _fileContents; }
+				set {
+					_fileContents = value;
+					ContentsDirty = true;
+				}
+			}
+			public bool ContentsDirty = false;
 
-            public bool busySaving = false;
+			public bool busySaving = false;
 
-            public ConfigFile(string rootPath, string configPath)
-            {
-                ConfigPath = configPath;
-                Name = new FileInfo(configPath).Name;
-                Enabled = File.Exists(Path.Combine(rootPath, "sites-enabled", Name));
-                _fileContents = File.ReadAllText(configPath);
-            }
+			public ConfigFile(string rootPath, string configPath)
+			{
+				ConfigPath = configPath;
+				Name = new FileInfo(configPath).Name;
+				Enabled = File.Exists(Path.Combine(rootPath, "sites-enabled", Name));
+				_fileContents = File.ReadAllText(configPath);
+			}
 
-            public async Task Save()
-            {
-                await Task.Run(() => File.WriteAllText(ConfigPath, _fileContents));
-                ContentsDirty = false;
-            }
-        }
+			public async Task Save()
+			{
+				await Task.Run(() => File.WriteAllText(ConfigPath, _fileContents));
+				ContentsDirty = false;
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Enums
+		#region Enums
 
-        public enum enuServiceStatus
-        {
-            Unknown,
-            Running,
-            Stopped,
-            Failed
-        }
+		public enum enuServiceStatus
+		{
+			Unknown,
+			Running,
+			Stopped,
+			Failed
+		}
 
-        public enum enuServiceAction
-        {
-            Start,
-            Stop,
-            Restart
-        }
+		public enum enuServiceAction
+		{
+			Start,
+			Stop,
+			Restart
+		}
 
-        #endregion
+		#endregion
 
-        #region Variables
+		#region Variables
 
-        private CLI _CLI;
+		private CLI _CLI;
 
-        private string _version = "";
-        private string _rootConfig = "";
-        private string _rootPath = "";
-        private enuServiceStatus _serviceStatus = enuServiceStatus.Unknown;
-        private string _serviceDetails = "";
+		private string _version = "";
+		private string _rootConfig = "";
+		private string _rootPath = "";
+		private enuServiceStatus _serviceStatus = enuServiceStatus.Unknown;
+		private string _serviceDetails = "";
 
-        private List<ConfigFile> _siteConfigs = new List<ConfigFile>();
+		private List<ConfigFile> _siteConfigs = new List<ConfigFile>();
 
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Properties
 
-        public bool Installed
-        {
-            get { return !(_rootConfig == ""); }
-        }
+		public bool Installed
+		{
+			get { return !(_rootConfig == ""); }
+		}
 
-        public string Version
-        {
-            get { return _version; }
-        }
+		public string Version
+		{
+			get { return _version; }
+		}
 
-        public enuServiceStatus ServiceStatus
-        {
-            get { return _serviceStatus; }
-        }
+		public enuServiceStatus ServiceStatus
+		{
+			get { return _serviceStatus; }
+		}
 
-        public string ServiceDetails
-        {
-            get { return _serviceDetails; }
-        }
+		public string ServiceDetails
+		{
+			get { return _serviceDetails; }
+		}
 
-        public string SitesAvailable
-        {
-            get { return Path.Combine(_rootPath, "sites-available"); }
-        }
+		public string SitesAvailable
+		{
+			get { return Path.Combine(_rootPath, "sites-available"); }
+		}
 
-        public List<ConfigFile> SiteConfigs
-        {
-            get { return _siteConfigs; }
-        }
+		public List<ConfigFile> SiteConfigs
+		{
+			get { return _siteConfigs; }
+		}
 
-        #endregion
+		#endregion
 
-        #region Constructors
+		#region Constructors
 
-        public Nginx(CLI CLI)
-        {
-            _CLI = CLI;
+		public Nginx(CLI CLI)
+		{
+			_CLI = CLI;
 
-            GetVersion();
-        }
+			Refresh();
+		}
 
-        #endregion
+		#endregion
 
-        public void GetVersion()
-        {
-            _version = "";
+		public void Refresh()
+		{
+			GetVersion();
+		}
 
-            _CLI.RunCommand("sudo", "nginx -V");
+		public void GetVersion()
+		{
+			_version = "";
 
-            Match match = new Regex("(?si)version:\\s(?<version>.*?)\\n.*--conf-path=(?<config>.*?)\\s").Match(_CLI.StandardError);
+			_CLI.RunCommand("sudo", "nginx -V");
 
-            if (match.Success)
-            {
-                _version = match.Groups["version"].Value;
-                _rootConfig = match.Groups["config"].Value;
-                _rootPath = new FileInfo(_rootConfig).DirectoryName ?? "";
+			Match match = new Regex("(?si)version:\\s(?<version>.*?)\\n.*--conf-path=(?<config>.*?)\\s").Match(_CLI.StandardError);
 
-                GetServiceStatus();
-                RefreshFiles();
-            }
-            else
-            {
-                _serviceStatus = enuServiceStatus.Unknown;
-            }
-        }
+			if (match.Success)
+			{
+				_version = match.Groups["version"].Value;
+				_rootConfig = match.Groups["config"].Value;
+				_rootPath = new FileInfo(_rootConfig).DirectoryName ?? "";
 
-        public void GetServiceStatus()
-        {
-            _CLI.RunCommand("sudo", "systemctl status nginx");
+				GetServiceStatus();
+				RefreshFiles();
+			}
+			else
+			{
+				_serviceStatus = enuServiceStatus.Unknown;
+			}
+		}
 
-            if (!string.IsNullOrWhiteSpace(_CLI.StandardError))
-            {
-                if (_CLI.StandardError == "Unit nginxd.service could not be found.")
-                {
-                    _serviceStatus = enuServiceStatus.Unknown;
-                }
-            }
-            else if (!string.IsNullOrWhiteSpace(_CLI.StandardOut))
-            {
-                _serviceDetails = string.Empty;
+		public void GetServiceStatus()
+		{
+			_CLI.RunCommand("sudo", "systemctl status nginx");
 
-                if (_CLI.StandardOut.Contains("Active: inactive"))
-                {
-                    _serviceStatus = enuServiceStatus.Stopped;
-                }
-                else if (_CLI.StandardOut.Contains("Active: active"))
-                {
-                    _serviceStatus = enuServiceStatus.Running;
-                }
-                else if (_CLI.StandardOut.Contains("Active: failed"))
-                {
-                    _serviceStatus = enuServiceStatus.Failed;
+			if (!string.IsNullOrWhiteSpace(_CLI.StandardError))
+			{
+				if (_CLI.StandardError == "Unit nginxd.service could not be found.")
+				{
+					_serviceStatus = enuServiceStatus.Unknown;
+				}
+			}
+			else if (!string.IsNullOrWhiteSpace(_CLI.StandardOut))
+			{
+				_serviceDetails = string.Empty;
 
-                    // Parse failures from the output
-                    Match match = new Regex(@"(?si)reverse proxy server\.\.\.(?<failures>.*?)\n[^\n]*nginx\.service").Match(_CLI.StandardOut);
+				if (_CLI.StandardOut.Contains("Active: inactive"))
+				{
+					_serviceStatus = enuServiceStatus.Stopped;
+				}
+				else if (_CLI.StandardOut.Contains("Active: active"))
+				{
+					_serviceStatus = enuServiceStatus.Running;
+				}
+				else if (_CLI.StandardOut.Contains("Active: failed"))
+				{
+					_serviceStatus = enuServiceStatus.Failed;
 
-                    if (match.Success)
-                    {
-                        _serviceDetails = "The following errors were returned by the service when attempting to start:<br />";
+					// Parse failures from the output
+					Match match = new Regex(@"(?si)reverse proxy server\.\.\.(?<failures>.*?)\n[^\n]*nginx\.service").Match(_CLI.StandardOut);
 
-                        string failures = match.Groups["failures"].Value.Trim();
+					if (match.Success)
+					{
+						_serviceDetails = "The following errors were returned by the service when attempting to start:<br />";
 
-                        // Attempt to match individual failures
-                        MatchCollection matches = new Regex(@"(?si)nginx:\s(?<failure>[^\n]*)").Matches(failures);
+						string failures = match.Groups["failures"].Value.Trim();
 
-                        if (matches.Count > 0)
-                        {
-                            for (int i = 0; i < matches.Count; i++)
-                            {
-                                _serviceDetails += "&emsp;" + (i+1).ToString() + ")&nbsp;" + matches[i].Groups["failure"].Value + "<br />";
-                            }
-                        }
-                        else
-                        {
-                            _serviceDetails += failures.Replace(Environment.NewLine, "<br />");
-                        }
-                    }
-                }
-            }
-        }
+						// Attempt to match individual failures
+						MatchCollection matches = new Regex(@"(?si)nginx:\s(?<failure>[^\n]*)").Matches(failures);
 
-        public void PerformServiceAction(enuServiceAction serviceAction)
-        {
-            if (serviceAction == enuServiceAction.Start)
-            {
-                _CLI.RunCommand("sudo", "systemctl start nginx");
-            }
-            else if (serviceAction == enuServiceAction.Stop)
-            {
-                _CLI.RunCommand("sudo", "systemctl stop nginx");
-            }
-            else if (serviceAction == enuServiceAction.Restart)
-            {
-                _CLI.RunCommand("sudo", "systemctl restart nginx");
-            }
+						if (matches.Count > 0)
+						{
+							for (int i = 0; i < matches.Count; i++)
+							{
+								_serviceDetails += "&emsp;" + (i+1).ToString() + ")&nbsp;" + matches[i].Groups["failure"].Value + "<br />";
+							}
+						}
+						else
+						{
+							_serviceDetails += failures.Replace(Environment.NewLine, "<br />");
+						}
+					}
+				}
+			}
+		}
 
-            GetServiceStatus();
-        }
+		public void PerformServiceAction(enuServiceAction serviceAction)
+		{
+			if (serviceAction == enuServiceAction.Start)
+			{
+				_CLI.RunCommand("sudo", "systemctl start nginx");
+			}
+			else if (serviceAction == enuServiceAction.Stop)
+			{
+				_CLI.RunCommand("sudo", "systemctl stop nginx");
+			}
+			else if (serviceAction == enuServiceAction.Restart)
+			{
+				_CLI.RunCommand("sudo", "systemctl restart nginx");
+			}
 
-        public string TestConfig()
-        {
-            _CLI.RunCommand("sudo", "nginx -t");
+			GetServiceStatus();
+		}
 
-            return _CLI.StandardOut + _CLI.StandardError;
-        }
+		public string TestConfig()
+		{
+			_CLI.RunCommand("sudo", "nginx -t");
 
-        public void RefreshFiles()
-        {
-            _siteConfigs.Clear();
+			return _CLI.StandardOut + _CLI.StandardError;
+		}
 
-            if (!string.IsNullOrWhiteSpace(_rootPath) && Directory.Exists(_rootPath))
-            {
-                foreach (string file in Directory.GetFiles(Path.Combine(_rootPath, "sites-available")))
-                {
-                    _siteConfigs.Add(new ConfigFile(_rootPath, file));
-                }
-                _siteConfigs.Sort((a,b) => a.Name.CompareTo(b.Name));
-            }
-        }
+		public void RefreshFiles()
+		{
+			_siteConfigs.Clear();
 
-        public void ToggleEnabled(ConfigFile config)
-        {
-            if (config.Enabled)
-            {
-                // Remove from sites-enabled
-                _CLI.RunCommand("sudo", "rm " + Path.Combine(_rootPath, "sites-enabled", config.Name));
-            }
-            else
-            {
-                // Add to sites-enabled
-                _CLI.RunCommand("sudo", "ln -s " + config.ConfigPath + " " + Path.Combine(_rootPath, "sites-enabled", config.Name));
-            }
+			if (!string.IsNullOrWhiteSpace(_rootPath) && Directory.Exists(_rootPath))
+			{
+				foreach (string file in Directory.GetFiles(Path.Combine(_rootPath, "sites-available")))
+				{
+					_siteConfigs.Add(new ConfigFile(_rootPath, file));
+				}
+				_siteConfigs.Sort((a,b) => a.Name.CompareTo(b.Name));
+			}
+		}
 
-            config.Enabled = !config.Enabled;
-        }
-    }
+		public void ToggleEnabled(ConfigFile config)
+		{
+			if (config.Enabled)
+			{
+				// Remove from sites-enabled
+				_CLI.RunCommand("sudo", "rm " + Path.Combine(_rootPath, "sites-enabled", config.Name));
+			}
+			else
+			{
+				// Add to sites-enabled
+				_CLI.RunCommand("sudo", "ln -s " + config.ConfigPath + " " + Path.Combine(_rootPath, "sites-enabled", config.Name));
+			}
+
+			config.Enabled = !config.Enabled;
+		}
+	}
 }
