8ba2735e7bf8b612d5a64a144be3898ae2e76f3a
SoulSeekkor <soulseekkor@live.com>
2024-07-04 11:20:47 -0500

    Created an app config builder to handle automatic self-signed certificate generation along with customization of other values such as port.

---
+++ AppConfig.cs
@@ -0,0 +1,133 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NginxPanel
+{
+    public class AppConfig
+    {
+#if DEBUG
+        private const string _basePath = "/mnt/d/Repositories/Visual Studio Projects/NginxPanel/config";
+#else
+        private const string _basePath = "/etc/nginxpanel";
+#endif
+        public const string AppConfigPath = _basePath + "/app.conf";
+
+        public static int Port { get; set; } = 5000;
+        public static string PFXPath { get; set; } = Path.Combine(_basePath, "self-signed.pfx");
+        public static string PFXPassword { get; set; } = GeneratePassword();
+
+        public static void Init()
+        {
+            // Check if config already exists
+            if (File.Exists(AppConfigPath))
+            {
+                // Load config
+                ReadConfig();
+            }
+            else
+            {
+                // Create a new config file with defaults
+                SaveConfig();
+            }
+        }
+
+        public static void SaveConfig()
+        {
+            if (!Directory.Exists(_basePath))
+                Directory.CreateDirectory(_basePath);
+
+            StringBuilder config = new StringBuilder();
+
+            config.AppendLine($"Port='{Port}'");
+            config.AppendLine($"PFXPath='{PFXPath}'");
+            config.AppendLine($"PFXPassword='{PFXPassword}'");
+
+            // Output config file (overwrites)
+            File.WriteAllText(AppConfigPath, config.ToString());
+
+            // Make sure certificate exists, if not then generate a new self-signed one
+            if (!File.Exists(PFXPath))
+                GenerateSelfSignedCert();
+        }
+
+        public static void ReadConfig()
+        {
+            string[] lines = File.ReadAllLines(AppConfigPath);
+            string[] split;
+
+            foreach (string line in lines)
+            {
+                split = line.Split('=');
+                split[1] = split[1].Trim('\'');
+
+                switch (split[0].ToLower())
+                {
+                    case "port":
+                        Port = int.Parse(split[1]); break;
+                    case "pfxpath":
+                        PFXPath = split[1]; break;
+                    case "pfxpassword":
+                        PFXPassword = split[1]; break;
+                }
+            }
+
+            // If PFX path is blank, generate a new self-signed one
+            // by updating and saving the default PFX path value
+            if (String.IsNullOrWhiteSpace(PFXPath))
+            {
+                PFXPath = Path.Combine(_basePath, "self-signed.pfx");
+                SaveConfig();
+            }
+        }
+
+        private static string GeneratePassword()
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder pass = new StringBuilder();
+            Random rnd = new Random();
+            for (int c = 0; c < 20; c++)
+            {
+                pass.Append(valid[rnd.Next(valid.Length)]);
+                Thread.Sleep(50);
+            }
+            return pass.ToString();
+        }
+
+        private static void GenerateSelfSignedCert()
+        {
+            FileInfo pfx = new FileInfo(PFXPath);
+
+            SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
+            sanBuilder.AddIpAddress(IPAddress.Loopback);
+            sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
+            sanBuilder.AddDnsName("localhost");
+            sanBuilder.AddDnsName(Environment.MachineName);
+
+            X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN=NginxPanel");
+
+            using (RSA rsa = RSA.Create(2048))
+            {
+                CertificateRequest request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+                request.CertificateExtensions.Add(
+                    new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
+
+                request.CertificateExtensions.Add(
+                   new X509EnhancedKeyUsageExtension(
+                       new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+                request.CertificateExtensions.Add(sanBuilder.Build());
+
+                X509Certificate2 certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+                byte[] certData = certificate.Export(X509ContentType.Pfx, PFXPassword);
+
+                if (!Directory.Exists(pfx.DirectoryName))
+                    Directory.CreateDirectory(pfx.DirectoryName!);
+
+                File.WriteAllBytes(pfx.FullName, certData);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,31 @@
 using NginxPanel.Services;
 using Radzen;
 using System.Net;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string certName = "self-signed.pfx";
-string certPass = "wXp3uOnHbLQSD3IrhGkY5MAXZsPDgiuC";
-#if DEBUG
-string certPath = "/mnt/d/Repositories/Visual Studio Projects/NginxPanel";
-#else
-string certPath = "/etc/nginxpanel";
-#endif
+// Initialize application configuration
+// NOTE: This creates files including self-signed.pfx!
+NginxPanel.AppConfig.Init();
 
-X509Certificate2? cert = null;
-
-if (!File.Exists(Path.Combine(certPath, certName)))
+// Verify port is set and PFX exists
+if (NginxPanel.AppConfig.Port <= 0)
 {
-	SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
-	sanBuilder.AddIpAddress(IPAddress.Loopback);
-	sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
-	sanBuilder.AddDnsName("localhost");
-	sanBuilder.AddDnsName(Environment.MachineName);
-
-	X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN=NginxPanel");
-
-	using (RSA rsa = RSA.Create(2048))
-	{
-		var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-		request.CertificateExtensions.Add(
-			new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
-
-		request.CertificateExtensions.Add(
-		   new X509EnhancedKeyUsageExtension(
-			   new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
-
-		request.CertificateExtensions.Add(sanBuilder.Build());
-
-		var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
-
-		byte[] certData = certificate.Export(X509ContentType.Pfx, certPass);
-		cert = new X509Certificate2(certData, certPass, X509KeyStorageFlags.MachineKeySet);
-
-		if (!Directory.Exists(certPath))
-			Directory.CreateDirectory(certPath);
-
-		File.WriteAllBytes(Path.Combine(certPath, certName), certData);        
-	}
+	// Fail on port not set
+	throw new Exception($"Must specify '{nameof(NginxPanel.AppConfig.Port)}' in the configuration file!");
 }
-else
+else if (!File.Exists(NginxPanel.AppConfig.PFXPath))
 {
-	cert = new X509Certificate2(Path.Combine(certPath, certName), certPass);
+	// Fail on non-existent PFX
+	throw new Exception($"Specified PFX file at '{NginxPanel.AppConfig.PFXPath}' does not exist! Clear this value in the config to create a new self-signed one.");
 }
 
 builder.WebHost.ConfigureKestrel((context, serverOptions) =>
 {
-	serverOptions.Listen(IPAddress.Any, 5000, listenOptions =>
+	serverOptions.Listen(IPAddress.Any, NginxPanel.AppConfig.Port, listenOptions =>
 	{
-		listenOptions.UseHttps(cert!);
+		listenOptions.UseHttps(new X509Certificate2(NginxPanel.AppConfig.PFXPath, NginxPanel.AppConfig.PFXPassword));
 	});
 });
 
